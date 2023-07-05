0aeffd29e5b44734bce5e29d04c808ea43dfbe79
Harsha <ksaisrinivasas@gmail.com>
2023-07-05 17:09:43 +0530

    New important files have been added.

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
 'use strict'
 
-const ReactElement = React.createElement('div', {class: "Parent"},[
-    React.createElement('div', {class: "child"},[
-        React.createElement('h1', {id: "heading"}, "Hello World from react"),
-        React.createElement('h2', {id: "subHeading"}, "Welcome to react")
-    ]
-    ),
-    React.createElement('div', {class: "child2"},[
-        React.createElement('h1', {id: "heading"}, "Hello World from react"),
-        React.createElement('h2', {id: "subHeading"}, "Welcome to react")
-]
-)
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(ReactElement);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 🚀
+
+
+# Parcel
+-Dev Build
+-Local server
+-HMR - Hot Module Replacement
+-File Watching Algorithm - written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-Differential Bundling - support older browsers
+-Diagonostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
